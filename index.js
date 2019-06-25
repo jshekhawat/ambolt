89ea0481e1ac46cb3a3ecf690bdb16011e19d578
jshekhawat <jitendersinghshekhawat@gmail.com>
2019-06-26 03:20:11 +0530

    add more options

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
 import program from 'commander'
 import {Network} from './network'
 
-program.command('create')
+program.command('create', 'creates a local network')
 .option('-c, --channels <channels>', 'number of channels in the network')
 .option('-o, --organizations <organizations>', 'number of organizations')
 .option('-u, --users <users>', 'number of users')
-.option()
     .action( async c => {
         const network = new Network()
         await network.create()
     })
 
-program.command('remove')
+program.command('remove', 'removes and cleans a local network')
     .action( async c => {
         await console.log(`network removed succesfully`)
 })
 
-program.command('attach')
+program.command('attach', 'registers a local copy of the chaincode to a remote peer for debug')
+	.option('-a, --address <address>', 'endpoint to the peer')
     .action(async c => {
         await console.log('attach allows to register the chaincode with a running peer')
     })
@@ -28,7 +28,5 @@ program.command('install')
     .action( async c => {
         await console.log('installing of chaincode would be supported in the future')
     })
-
-
 
 program.parse(process.argv)
